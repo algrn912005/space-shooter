602777cd2e530fb6e7bb50e752c8fc07014ac283
Alvin Lee <algrn91@gmail.com>
2019-12-01 20:33:56 -0800

    Added showing of high scores

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -1,14 +1,16 @@
 extends Node
 
+const HIGHSCORE_DATA = "user://highscores.data"
+
 var world = null
 var score = 0
 
 var highscore = [
-    ["", 0],
-    ["", 0],
-    ["", 0],
-    ["", 0],
-    ["", 0]
+    ["ANON", 0],
+    ["ANON", 0],
+    ["ANON", 0],
+    ["ANON", 0],
+    ["ANON", 0]
 ]
 
 
@@ -21,23 +23,26 @@ func add_child_to_world(inst) -> void:
         world.add_child(inst)          
 
 
-func load_high_scores() -> void:
+func load_high_scores() -> Array:
     var in_file = File.new()
-    in_file.open("user://highscores.data", File.READ)
-    var hs_data = parse_json(in_file.get_as_text())
-    highscore = hs_data
+    if in_file.file_exists(HIGHSCORE_DATA):
+        in_file.open(HIGHSCORE_DATA, File.READ)
+        var hs_data = parse_json(in_file.get_as_text())
+        highscore = hs_data
     in_file.close()
+    
+    return highscore
 
 
 func store_high_score() -> void:
     var out_file = File.new()
-    out_file.open("user://highscores.data", File.WRITE)
+    out_file.open(HIGHSCORE_DATA, File.WRITE)
     out_file.store_line(to_json(highscore))
     out_file.close()
     
 
 func process_high_score(name: ="") -> void:
-    highscore.append(["", score])
+    highscore.append(["Alvin", score])
     highscore.sort_custom(HighScoreSorter, "sort")           
     highscore.pop_back()
 
@@ -47,5 +52,3 @@ class HighScoreSorter:
         if a[1] > b[1]:
             return true
         return false
-
-
--- scripts/Title.gd
+++ scripts/Title.gd
@@ -2,18 +2,33 @@ extends CanvasLayer
 
 
 func _ready():
-    Globals.load_high_scores()
+    var scores = Globals.load_high_scores()
     $ShowTitleAnim.play("show_title")
+    set_scores(scores)
+    $Highscores/Scores/ShowScores.play("ShowScore")
 
 
-func _on_StartButton_pressed():
-# warning-ignore:return_value_discarded
+func _on_StartButton_pressed() -> void:
+    # warning-ignore:return_value_discarded
     get_tree().change_scene("res://scenes/World.tscn")
 
 
-func _on_QuitButton_pressed():
+func _on_QuitButton_pressed() -> void:
     get_tree().quit()
 
 
-func _on_RepeatTitleAnim_timeout():
+func _on_RepeatTitleAnim_timeout() -> void:
     $ShowTitleAnim.play("show_title")
+
+
+func set_scores(scores: Array) -> void:
+    # Sets the scores in the 'Scores' labal
+    var text = ""
+    for i in range(5):
+        text += (
+            str(i + 1) + ". " 
+            + str(scores[i][0]) + " - " 
+            + str(scores[i][1]) + "\n"
+        )
+        
+    $Highscores/Scores.text = text
